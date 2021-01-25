10cad840063f1f65c8dc48ec7809c2a2f648f4cb
Dmitriy Antonov <79038945169@yandex.ru>
2021-01-25 18:23:38 +0300

    update

--- app/src/main/java/com/example/wsr21/MainActivity.java
+++ app/src/main/java/com/example/wsr21/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
         TextView date = (TextView)findViewById(R.id.textDate);
         SimpleDateFormat format = new SimpleDateFormat("dd.mm.yyyy");
         date.setText(format.format(new Date()));
+        Button buttonBanki = (Button)findViewById(R.id.button);
+        buttonBanki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"банки",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        LinearLayout buttonMix = (LinearLayout)findViewById(R.id.buttonMix);
+        buttonMix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"курсы валют",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
