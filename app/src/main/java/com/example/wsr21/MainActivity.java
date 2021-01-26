d251db54054d69d1db40d759fa35289ec756f416
Dmitriy Antonov <79038945169@yandex.ru>
2021-01-26 09:23:00 +0300

    update

--- app/src/main/java/com/example/wsr21/MainActivity.java
+++ app/src/main/java/com/example/wsr21/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.wsr21;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Context context = this;
         TextView date = (TextView)findViewById(R.id.textDate);
         SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         date.setText(format.format(new Date()));
@@ -34,6 +39,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"курсы валют",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //создаем всплывающее окно авторизации
+        //кнопка авторизации
+        Button butt_auth = (Button)findViewById(R.id.button5);
+        butt_auth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //берем разметку для окна авторизации
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.auth_wind, null);
+                //Создаем AlertDialog
+                AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(context);
+                //Настраиваем prompt.xml для нашего AlertDialog:
+                mDialogBuilder.setView(promptsView);
+                //Настраиваем отображение поля для ввода текста в открытом диалоге:
+                final EditText userInputLogin = (EditText) promptsView.findViewById(R.id.login_txt);
+                final EditText userInputPassword = (EditText) promptsView.findViewById(R.id.password_txt);
             }
         });
     }
